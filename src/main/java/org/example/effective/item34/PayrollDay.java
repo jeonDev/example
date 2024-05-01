b2e8206d42919d44cbf2ca57b94272c10a895b47
jeonDev <jjhwhdgus0727@gmail.com>
2024-05-01 21:31:52 +0900

    Effective Java

---
+++ src/main/java/org/example/effective/item34/PayrollDay.java
@@ -0,0 +1,23 @@
+package org.example.effective.item34;
+
+import static org.example.effective.item34.PayType.WEEKDAY;
+import static org.example.effective.item34.PayType.WEEKEND;
+
+public enum PayrollDay {
+    MONDAY(WEEKDAY),
+    TUESDAY(WEEKDAY),
+    WEDNESDAY(WEEKDAY),
+    THURSDAY(WEEKDAY),
+    FRIDAY(WEEKDAY),
+    SATURDAY(WEEKEND),
+    SUNDAY(WEEKEND);
+
+    private final PayType payType;
+
+    PayrollDay(PayType payType) { this.payType = payType; }
+
+    int pay(int minutesWorked, int payRate) {
+        return payType.pay(minutesWorked, payRate);
+    }
+
+}
