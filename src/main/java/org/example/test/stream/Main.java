08b08a5c9ca71303d8f9f01e52b7c4a2dc883181
jeonDev <jjhwhdgus0727@gmail.com>
2024-06-30 13:56:14 +0900

    자바 전월부터 현월 데이터 처리 3 (코드 추가)

--- src/main/java/org/example/test/stream/Main.java
+++ src/main/java/org/example/test/stream/Main.java
@@ -25,30 +25,33 @@ public class Main {
                 new Data("20240601", "2", BigDecimal.TEN),
                 new Data("20240602", "1", BigDecimal.TEN)
         );
+        List<String> divisionCodeList = List.of("2", "3", "6");
 
         LocalDate currentDate = LocalDate.now();
         LocalDate firstDayOfLastMonth = currentDate.minusMonths(1).withDayOfMonth(1);
         YearMonth currentYearMonth = YearMonth.from(currentDate);
         LocalDate lastDayOfCurrentMonth = currentYearMonth.atEndOfMonth();
         LocalDate date = firstDayOfLastMonth;
-        Dttm dttm = null;
         while (!date.isAfter(lastDayOfCurrentMonth)) {
-            dttm = new Dttm(date.format(formatter));
-            dttmList.add(dttm);
+            String format = date.format(formatter);
+            List<Dttm> collect = divisionCodeList.stream()
+                    .map(s -> new Dttm(format, s))
+                    .collect(Collectors.toList());
+            dttmList.addAll(collect);
             date = date.plusDays(1);
         }
 
-        Map<String, List<Data>> collect = dataList.stream()
-                .collect(Collectors.groupingBy(Data::getDate));
+        Map<String, Map<String, Data>> collect = dataList.stream()
+                .collect(Collectors.groupingBy(Data::getDate,
+                        Collectors.toMap(Data::getDivisionCode, payment -> payment)));
 
         dttmList.stream()
                 .flatMap(d -> {
-                    List<Data> datas = collect.getOrDefault(d.getDate(), Collections.emptyList());
-                    if (datas.isEmpty()) {
-                        return Stream.of(new Data(d.getDate(), "1", BigDecimal.ZERO));
+                    Data data = collect.getOrDefault(d.getDate(), Collections.emptyMap()).get(d.getDivisionCode());
+                    if (data == null) {
+                        return Stream.of(new Data(d.getDate(), d.getDivisionCode(), BigDecimal.ZERO));
                     } else {
-                        return datas.stream()
-                                .map(p -> new Data(d.getDate(), p.getDivisionCode(), p.getAmount()));
+                        return Stream.of(data);
                     }
                 })
                 .toList()
@@ -57,13 +60,19 @@ public class Main {
 
     static class Dttm {
         private String date;
+        private String divisionCode;
 
-        public Dttm(String date) {
+        public Dttm(String date, String divisionCode) {
             this.date = date;
+            this.divisionCode = divisionCode;
         }
 
         public String getDate() {
             return date;
+        }
+
+        public String getDivisionCode() {
+            return divisionCode;
         }
 
         @Override
